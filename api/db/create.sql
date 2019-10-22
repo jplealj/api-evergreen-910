765635c57bdb7d70d34b906c69cadb5a46c50ce3
jplealj <37347503+jplealj@users.noreply.github.com>
2019-10-22 16:48:43 -0500

    Delete create.sql

--- api/db/create.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `evergreen`.`mediciones` (
-  `fecha` DATETIME NOT NULL,
-  `origen` VARCHAR(100) NOT NULL,
-  `valor` INT NULL,
-  `codigoSensor` INT NOT NULL,
-  `observacion` VARCHAR(100) NULL);
