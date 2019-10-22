f5f494bb952757c806963b4213a59c7c25676122
jplealj <37347503+jplealj@users.noreply.github.com>
2019-10-22 16:48:52 -0500

    Delete db.py

--- api/db/db.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector as mysql
-cnx = mysql.MySQLConnection(
-    host = "db-server-evergreen-jpl.mysql.database.azure.com",
-    port = 3306,
-    user = "evergreenadmin@db-server-evergreen-jpl",
-    password = "qrxGAA19",
-    database = "evergreen",
-)
